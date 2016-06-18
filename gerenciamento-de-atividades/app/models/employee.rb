d5f52185592a9af8c498a4f573ddd3b14b260161
Izabela Cardoso <cizabelacristina@gmail.com>
2016-06-18 17:56:32 -0300

    Issue #4: Editing view of employee

--- gerenciamento-de-atividades/app/models/employee.rb
+++ gerenciamento-de-atividades/app/models/employee.rb
@@ -1,2 +1,7 @@
 class Employee < ActiveRecord::Base
+	validates :name, length: { maximum: 100, too_long: "O nome deve ter no máximo %{count} caracteres"}
+	validates_presence_of :name, message: "O nome não pode ser vazio"
+	validates_presence_of :enrollment, message: "A matrícula não pode ser vazia"
+	validates_presence_of :cpf, message: "O cpf não pode ser vazio"
+	validates_presence_of :phone, message: "O telefone não pode ser vazio"
 end
